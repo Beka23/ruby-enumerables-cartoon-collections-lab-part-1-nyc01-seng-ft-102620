ce7d17902e8a619b4796e2f229119befeae1c6bc
Beksultan <beksultankaiypov@gmail.com>
2020-10-14 20:37:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,7 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   
-  array.each_with_index do |names,index+1|
+  array.each_with_index do |names|
     puts names
   end
 end
